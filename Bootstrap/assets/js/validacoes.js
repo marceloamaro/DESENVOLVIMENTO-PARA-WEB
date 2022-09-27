83dcab9a7e53d2de952d1ed2676064188c991b8f
marceloamaro <marcelo.amaro@aluno.uece.br>
2022-09-27 20:40:58 -0300

    progWeb

--- Bootstrap/assets/js/validacoes.js
+++ Bootstrap/assets/js/validacoes.js
@@ -2,4 +2,11 @@ function validarCadastro(){
    let nome = document.getElementById('nome').value;
    let email = document.getElementById('email').value;
    let senha = document.getElementById('senha').value;
+   let estado = document.getElementById('estado').value;
+
+   if(nome ==""){
+       document.getElementById('erro').innerHTML = "<strong>OPS! Informe seu nome.</strong>";
+       document.getElementById('erro').removeAttribute('hidden');
+       document.getElementById('nome').focus();
+   }
 }
